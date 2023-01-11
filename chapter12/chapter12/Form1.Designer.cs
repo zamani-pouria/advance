91db5457828d97e68e05aa674f3877d27ba3f844
Pouria <79441285+zamani-pouria@users.noreply.github.com>
2023-01-12 02:58:08 +0330

    Add files via upload

---
+++ chapter12/chapter12/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace chapter12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.id = new System.Windows.Forms.TextBox();
+            this.fname = new System.Windows.Forms.TextBox();
+            this.lname = new System.Windows.Forms.TextBox();
+            this.sallary = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // id
+            // 
+            this.id.Location = new System.Drawing.Point(105, 28);
+            this.id.Name = "id";
+            this.id.Size = new System.Drawing.Size(100, 22);
+            this.id.TabIndex = 0;
+            // 
+            // fname
+            // 
+            this.fname.Location = new System.Drawing.Point(105, 74);
+            this.fname.Name = "fname";
+            this.fname.Size = new System.Drawing.Size(100, 22);
+            this.fname.TabIndex = 1;
+            // 
+            // lname
+            // 
+            this.lname.Location = new System.Drawing.Point(105, 122);
+            this.lname.Name = "lname";
+            this.lname.Size = new System.Drawing.Size(100, 22);
+            this.lname.TabIndex = 2;
+            // 
+            // sallary
+            // 
+            this.sallary.Location = new System.Drawing.Point(105, 169);
+            this.sallary.Name = "sallary";
+            this.sallary.Size = new System.Drawing.Size(100, 22);
+            this.sallary.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(119, 224);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "حساب";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "emploii id";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "first name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "last name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 172);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "sallary";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(278, 300);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.sallary);
+            this.Controls.Add(this.lname);
+            this.Controls.Add(this.fname);
+            this.Controls.Add(this.id);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox id;
+        private System.Windows.Forms.TextBox fname;
+        private System.Windows.Forms.TextBox lname;
+        private System.Windows.Forms.TextBox sallary;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ chapter12/chapter12/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chapter12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double tax=0, insurance=0, endsallary=0,cid,csallary;
+            string cfname, clname, message;
+
+            if(id.Text != "" && fname.Text != "" && lname.Text != "" && sallary.Text != "" )
+            {
+                cid = Convert.ToDouble(id.Text);
+                csallary = Convert.ToDouble(sallary.Text);
+                cfname = fname.Text;
+                clname = lname.Text;
+                if(csallary>6500)
+                {
+                    tax = (csallary * 10) / 100;
+                }
+                insurance = (csallary * 7) / 100;
+                endsallary = csallary - (tax + insurance);
+                message= ("hi dear "+ cfname+" "+ clname+" with id "+ cid+". your sallary without insurance and tax is: "+ endsallary);
+                MessageBox.Show(message);
+
+            }
+        }
+    }
+}
